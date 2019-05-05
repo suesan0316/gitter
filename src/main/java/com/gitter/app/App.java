84169ff9884f75592469a04827a2f88c590901bf
ShunsukeSuzuki0316 <shunsuke.suzuki0316@gmail.com>
2019-05-05 13:47:54 +0900

    Add More System out println

--- src/main/java/com/gitter/app/App.java
+++ src/main/java/com/gitter/app/App.java
@@ -11,5 +11,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
         System.out.println("My Git PullRequest");
+        System.out.println("My Git PullRequest2");
     }
 }
